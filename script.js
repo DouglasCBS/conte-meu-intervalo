58c7262cd83fea46fac2f27593b54cfa2e0aed3e
DouglasCBS <douglascesar12@gmail.com>
2024-01-20 21:13:38 -0300

    all done yay

---
+++ script.js
@@ -0,0 +1,38 @@
+const mtime = document.getElementById("time");
+const itime = document.getElementById("timeI");
+const calcTime = document.getElementById("calcTime");
+const header = document.querySelector("h1");
+
+calcTime.addEventListener("click", () => {
+    let cTime = mtime.value
+    let iTime = itime.value;
+    let result = getTime(cTime, iTime)
+    header.textContent = `${result}`;
+});
+
+function getTime(time, stime) {
+    let hour = parseInt(time.slice(0, 2));
+    let minutes = parseInt(time.slice(3, 5));
+    let sHour = parseInt(stime.slice(0, 2));
+    let sMinutes = parseInt(stime.slice(3,5));
+
+    let cHour = hour + sHour;
+    let cMinutes = minutes + sMinutes;
+
+    if (cMinutes < 60) {
+        return `Teu intervalo acaba ${cHour}` + ":" + `${cMinutes}`
+    }
+    else if (cMinutes >= 60) {
+        let sobra = cMinutes % 60;
+        let minutes = Math.floor(cMinutes / 60);
+        cHour = cHour + minutes;
+        cMinutes = sobra;
+        if (cMinutes < 10) {
+            return `Teu intervalo acaba ${cHour}` + ":" + `0${cMinutes}`
+        }
+        else if (cMinutes >= 10) {
+            return `Teu intervalo acaba ${cHour}` + ":" + `${cMinutes}`
+        }
+    }
+    
+}
